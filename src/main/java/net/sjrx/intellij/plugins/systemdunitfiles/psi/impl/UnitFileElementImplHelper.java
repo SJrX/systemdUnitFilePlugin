5e977838a5ee691616a20f9def6f6978d10f8a29
Vladislav Rassokhin <vladrassokhin@gmail.com>
2020-03-14 14:36:32 +0300

    Cleanup

--- src/main/java/net/sjrx/intellij/plugins/systemdunitfiles/psi/impl/UnitFileElementImplHelper.java
+++
@@ -1,9 +0,0 @@
-package net.sjrx.intellij.plugins.systemdunitfiles.psi.impl;
-
-import com.intellij.lang.ASTNode;
-import net.sjrx.intellij.plugins.systemdunitfiles.generated.UnitFileElementTypeHolder;
-import net.sjrx.intellij.plugins.systemdunitfiles.psi.UnitFileProperty;
-
-public class UnitFileElementImplHelper {
-
-}
