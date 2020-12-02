17d255ade7a8b9c28e6d5b9782720f21b2bd77b6
Steve Ramage <gitcommits@sjrx.net>
2020-12-02 10:14:26 -0800

    Updates to IntelliJ 2020.3, Java 11, and Systemd v247.

--- src/test/java/net/sjrx/intellij/plugins/systemdunitfiles/commenter/UnitFileCommenterTest.java
+++ src/test/java/net/sjrx/intellij/plugins/systemdunitfiles/commenter/UnitFileCommenterTest.java
@@ -1,10 +1,10 @@
 package net.sjrx.intellij.plugins.systemdunitfiles.commenter;
 
 import com.intellij.codeInsight.generation.actions.CommentByLineCommentAction;
-import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 import net.sjrx.intellij.plugins.systemdunitfiles.filetypes.ServiceFileType;
 
-public class UnitFileCommenterTest extends LightPlatformCodeInsightFixtureTestCase {
+public class UnitFileCommenterTest extends BasePlatformTestCase {
 
   public void testCommenter() {
     myFixture.configureByText(ServiceFileType.INSTANCE, "<caret>[Service]");
