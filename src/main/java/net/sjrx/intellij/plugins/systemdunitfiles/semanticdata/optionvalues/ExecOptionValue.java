0233c0193b1878f2155aa535abdf28386b6339f2
Steve Ramage <gitcommits@sjrx.net>
2022-06-03 12:59:10 -0700

    Resolves #133 - Adds warning when there is no absolute path specified for Exec options

--- src/main/java/net/sjrx/intellij/plugins/systemdunitfiles/semanticdata/optionvalues/ExecOptionValue.java
+++ src/main/java/net/sjrx/intellij/plugins/systemdunitfiles/semanticdata/optionvalues/ExecOptionValue.java
@@ -1,10 +1,17 @@
 package net.sjrx.intellij.plugins.systemdunitfiles.semanticdata.optionvalues;
 
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+import net.sjrx.intellij.plugins.systemdunitfiles.psi.UnitFilePropertyType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
 import java.util.Set;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This validator is used for the config_parse_exec validator.
@@ -14,9 +21,37 @@ import java.util.Set;
  */
 public class ExecOptionValue implements OptionValueInformation {
   
+  private static final Pattern RELATIVE_PATH_REGEX = Pattern.compile("^\\s*([^\\/\\s]\\S*)\\s*");
+  
+  
   @Override
   public Set<String> getAutoCompleteOptions(@NotNull Project project) {
     return Collections.emptySet();
+  }
+  
+  /**
+   * Returns problem descriptors.
+   * @param property - the Psi Element we are examining.
+   * @param holder - A problem holder that we should add to.
+   */
+  public void generateProblemDescriptors(@NotNull UnitFilePropertyType property, @NotNull ProblemsHolder holder) {
+  
+    String values = property.getValueNode().getText();
+    
+    Matcher m = RELATIVE_PATH_REGEX.matcher(values);
+    
+    if (m.find()) {
+      MatchResult mr = m.toMatchResult();
+      
+      TextRange range = new TextRange(mr.start(1), mr.end(1));
+      
+      holder.registerProblem(
+        property.getValueNode().getPsi(),
+        "It is recommended to use an absolute path to avoid ambiguity. If the command is not a full (absolute) path, it"
+        + " will be resolved to a full path using a fixed search path (that depends on the distribution) determined at compilation time.",
+        ProblemHighlightType.WEAK_WARNING, range);
+    }
+    
   }
   
   @Override
