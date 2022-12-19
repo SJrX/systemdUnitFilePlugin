ef543b6026dcfb55f6e18ffd543fc84082c2df07
Steve Ramage <gitcommits@sjrx.net>
2022-12-19 15:39:31 -0800

    Move icon package over to Kotlin

---
+++ src/main/kotlin/net/sjrx/intellij/plugins/systemdunitfiles/UnitFileIcon.kt
@@ -0,0 +1,8 @@
+package net.sjrx.intellij.plugins.systemdunitfiles
+
+import com.intellij.openapi.util.IconLoader
+
+object UnitFileIcon {
+  @JvmField
+  val FILE = IconLoader.getIcon("/net/sjrx/intellij/plugins/systemdunitfiles/systemd.svg", UnitFileIcon::class.java)
+}
