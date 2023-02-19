edf4c4454d57a9f5943291f12fae2c6b0824da3f
Steve Ramage <gitcommits@sjrx.net>
2023-02-19 10:57:52 -0800

    Resolves #198 - Add warning on PIDFile usage

---
+++ src/main/kotlin/net/sjrx/intellij/plugins/systemdunitfiles/annotators/PidFileOptionWarning.kt
@@ -0,0 +1,25 @@
+package net.sjrx.intellij.plugins.systemdunitfiles.annotators
+
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import net.sjrx.intellij.plugins.systemdunitfiles.psi.UnitFileProperty
+import net.sjrx.intellij.plugins.systemdunitfiles.psi.UnitFileSectionType
+
+class PidFileOptionWarning : Annotator {
+
+  override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+    if (element is UnitFileProperty) {
+      val section = PsiTreeUtil.getParentOfType(element, UnitFileSectionType::class.java)
+      if (element.key == "PIDFile" && section?.sectionName == "Service") {
+        holder.newAnnotation(HighlightSeverity.WEAK_WARNING, ANNOTATION_ERROR_MSG).range(element.keyNode.psi).create()
+      }
+    }
+  }
+
+    companion object {
+      const val ANNOTATION_ERROR_MSG = "PID files should be avoided in modern projects. Use type=notify, Type=notify-release or Type=simple  where possible, which does not require use of PID files to determine the main process of a service and avoids needless forking."
+    }
+}
