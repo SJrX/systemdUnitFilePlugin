79f8737c4591dd9927df70b57b0a3bb13863bb93
Vladislav Rassokhin <vladrassokhin@gmail.com>
2024-01-17 13:13:40 +0100

    Add progress logging to long-running ubuntu-units.sh

--- systemd-build/ubuntu-units.sh
+++ systemd-build/ubuntu-units.sh
@@ -2,5 +2,8 @@
 
 for type in service socket device mount automount swap target path timer slice scope
 do
+  echo "Processing extension '$type'" >&2
   apt-file search -x  "\.$type$" | grep -v example | grep "systemd"  | grep -v "/test" | sed -E "s#^.+/##g" | sort | uniq
 done
+
+echo "All done" >&2
