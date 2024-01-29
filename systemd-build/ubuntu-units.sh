39d063da9c9bc12be5bc513fac5ce6dff532d098
Steve Ramage <github@sjrx.net>
2024-01-29 11:05:49 -0800

    Revert "Add progress logging to long-running ubuntu-units.sh"
    This reverts commit f3e995a6ef188ac0a18ca199df18b09391262751.

--- systemd-build/ubuntu-units.sh
+++ systemd-build/ubuntu-units.sh
@@ -2,8 +2,5 @@
 
 for type in service socket device mount automount swap target path timer slice scope
 do
-  echo "Processing extension '$type'" >&2
   apt-file search -x  "\.$type$" | grep -v example | grep "systemd"  | grep -v "/test" | sed -E "s#^.+/##g" | sort | uniq
 done
-
-echo "All done" >&2
