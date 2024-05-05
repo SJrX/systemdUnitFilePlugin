37f158ff7528d33427efcc8c7472e98b46ee67e5
Steve Ramage <gitcommits@sjrx.net>
2024-05-05 14:26:40 -0700

    Resolves #244 - Make builds compatible with systemd v256

--- systemd-build/Systemd-Builder.Dockerfile
+++ systemd-build/Systemd-Builder.Dockerfile
@@ -6,13 +6,14 @@ RUN ln -fs /usr/share/zoneinfo/America/Vancouver /etc/localtime
 
 RUN apt-get update && apt-get -y install git build-essential tzdata meson pkg-config gperf python3-jinja2 libcap-dev util-linux libmount1 libmount-dev
 
+
 RUN mkdir /opt/systemd-source
 
 RUN git clone https://github.com/systemd/systemd.git /opt/systemd-source/systemd
 
 WORKDIR /opt/systemd-source/systemd
 
-RUN ./configure
+RUN meson setup build
 
 RUN mkdir -p /mount/
 
--- systemd-build/systemd-build.sh
+++ systemd-build/systemd-build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+
 echo "Git Pull" && \
   git pull && \
   echo "Run jinja2" && \
-  python3 ./tools/meson-render-jinja2.py ./build/config.h ./src/version/version.h.in  ./src/core/load-fragment-gperf.gperf.in load-fragment-gperf.gperf &&  \
+  python3 ./tools/meson-render-jinja2.py ./build/config.h ./src/core/load-fragment-gperf.gperf.in load-fragment-gperf.gperf &&  \
   echo "Copy file(s)" && \
   cp load-fragment-gperf.gperf /mount/load-fragment-gperf.gperf && \
   cp -R ./man /mount/ && \
+  git log -1 --format="%at" | xargs -I{} date -d @{} +%Y-%m-%d > /mount/last_commit_date && \
   echo "Reset Permissions" && \
   chmod 777 -R /mount
