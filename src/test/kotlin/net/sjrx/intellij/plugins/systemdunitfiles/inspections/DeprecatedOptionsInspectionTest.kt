10abddad4809106f687d68987f0954af0e714f85
Steve Ramage <github@sjrx.net>
2025-05-03 08:33:47 -0700

    ci: remove tests that were causing failing builds due to removal of cgroupv1 options in systemd (#353)
    Co-authored-by: Steve Ramage <gitcommits@sjrx.net>

--- src/test/kotlin/net/sjrx/intellij/plugins/systemdunitfiles/inspections/DeprecatedOptionsInspectionTest.kt
+++ src/test/kotlin/net/sjrx/intellij/plugins/systemdunitfiles/inspections/DeprecatedOptionsInspectionTest.kt
@@ -43,7 +43,7 @@ class DeprecatedOptionsInspectionTest : AbstractUnitFileTest() {
   fun testSingleExampleThrowsWarning() {
     val file = """
            [Service]
-           MemoryLimit=8
+           InaccessibleDirectories=/boo
            """.trimIndent()
 
     // Exercise SUT
@@ -57,144 +57,10 @@ class DeprecatedOptionsInspectionTest : AbstractUnitFileTest() {
     // Verification
     assertSize(1, highlights)
     val info = highlights[0]
-    TestCase.assertEquals("'MemoryLimit' in section 'Service' has been renamed to 'MemoryMax'", info!!.description)
+    TestCase.assertEquals("'InaccessibleDirectories' in section 'Service' has been renamed to 'InaccessiblePath'", info!!.description)
     val highlightElement = myFixture.file.findElementAt(info.getStartOffset())
     TestCase.assertNotNull(highlightElement)
-    TestCase.assertEquals("MemoryLimit", highlightElement!!.text)
+    TestCase.assertEquals("InaccessibleDirectories", highlightElement!!.text)
   }
 
-  fun testAllDocumentedDeprecatedOptionsInServiceAsOfV240ThrowsWarning() {
-    val file = """
-           [Service]
-           CPUShares=52
-           StartupCPUShares=25
-           MemoryLimit=8
-           BlockIOAccounting=1
-           BlockIOWeight=25
-           StartupBlockIOWeight=25
-           BlockIODeviceWeight=25
-           BlockIOReadBandwidth=2552
-           BlockIOWriteBandwidth=252
-           
-           """.trimIndent()
-
-    // Exercise SUT
-    setupFileInEditor("file.service", file)
-    enableInspection(DeprecatedOptionsInspection::class.java)
-
-    // Verification
-    val highlights = myFixture.doHighlighting()
-
-
-    // Verification
-    assertSize(9, highlights)
-  }
-
-  fun testAllDocumentedDeprecatedOptionsInMountAsOfV240ThrowsWarning() {
-    val file = """
-           [Mount]
-           CPUShares=52
-           StartupCPUShares=25
-           MemoryLimit=8
-           BlockIOAccounting=1
-           BlockIOWeight=25
-           StartupBlockIOWeight=25
-           BlockIODeviceWeight=25
-           BlockIOReadBandwidth=2552
-           BlockIOWriteBandwidth=252
-           
-           """.trimIndent()
-
-    // Exercise SUT
-    setupFileInEditor("file.mount", file)
-    enableInspection(DeprecatedOptionsInspection::class.java)
-
-    // Verification
-    val highlights = myFixture.doHighlighting()
-
-
-    // Verification
-    assertSize(9, highlights)
-  }
-
-  fun testAllDocumentedDeprecatedOptionsInSocketAsOfV240ThrowsWarning() {
-    val file = """
-           [Socket]
-           CPUShares=52
-           StartupCPUShares=25
-           MemoryLimit=8
-           BlockIOAccounting=1
-           BlockIOWeight=25
-           StartupBlockIOWeight=25
-           BlockIODeviceWeight=25
-           BlockIOReadBandwidth=2552
-           BlockIOWriteBandwidth=252
-           
-           """.trimIndent()
-
-    // Exercise SUT
-    setupFileInEditor("file.socket", file)
-    enableInspection(DeprecatedOptionsInspection::class.java)
-
-    // Verification
-    val highlights = myFixture.doHighlighting()
-
-
-    // Verification
-    assertSize(9, highlights)
-  }
-
-  fun testAllDocumentedDeprecatedOptionsInSwapAsOfV240ThrowsWarning() {
-    val file = """
-           [Swap]
-           CPUShares=52
-           StartupCPUShares=25
-           MemoryLimit=8
-           BlockIOAccounting=1
-           BlockIOWeight=25
-           StartupBlockIOWeight=25
-           BlockIODeviceWeight=25
-           BlockIOReadBandwidth=2552
-           BlockIOWriteBandwidth=252
-           
-           """.trimIndent()
-
-    // Exercise SUT
-    setupFileInEditor("file.swap", file)
-    enableInspection(DeprecatedOptionsInspection::class.java)
-
-    // Verification
-    val highlights = myFixture.doHighlighting()
-
-
-    // Verification
-    assertSize(9, highlights)
-  }
-
-  fun testAllDocumentedDeprecatedOptionsInSliceAsOfV240ThrowsWarning() {
-    val file = """
-           [Slice]
-           CPUShares=52
-           StartupCPUShares=25
-           MemoryLimit=8
-           BlockIOAccounting=1
-           BlockIOWeight=25
-           StartupBlockIOWeight=25
-           BlockIODeviceWeight=25
-           BlockIOReadBandwidth=2552
-           BlockIOWriteBandwidth=252
-           
-           """.trimIndent()
-
-    // Exercise SUT
-    setupFileInEditor("file.slice", file)
-    enableInspection(DeprecatedOptionsInspection::class.java)
-
-    // Verification
-    val highlights = myFixture.doHighlighting()
-
-
-    // Verification
-    assertSize(9, highlights)
-  }
 }
