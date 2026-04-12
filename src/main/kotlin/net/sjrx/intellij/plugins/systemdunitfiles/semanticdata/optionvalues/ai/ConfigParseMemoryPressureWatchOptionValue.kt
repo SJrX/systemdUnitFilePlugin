db36f493ef4ed6a710e1c0580961c9612434b34c
Steve Ramage <gitcommits@sjrx.net>
2026-04-12 09:54:08 -0700

    fix: fix support for MemoryPressureWatch= after update

--- src/main/kotlin/net/sjrx/intellij/plugins/systemdunitfiles/semanticdata/optionvalues/ai/ConfigParseMemoryPressureWatchOptionValue.kt
+++ src/main/kotlin/net/sjrx/intellij/plugins/systemdunitfiles/semanticdata/optionvalues/ai/ConfigParseMemoryPressureWatchOptionValue.kt
@@ -6,13 +6,13 @@ import net.sjrx.intellij.plugins.systemdunitfiles.semanticdata.optionvalues.Simp
 
 /**
  * Validator for Swap.MemoryPressureWatch
- * C Function: config_parse_memory_pressure_watch(0)
+ * C Function: config_parse_pressure_watch(0)
  * Used by Options: Swap.MemoryPressureWatch
- * 
+ *
  * Accepts boolean values (yes/no/1/0/true/false/on/off/y/n/t/f) or special values (auto/skip).
  */
 class ConfigParseMemoryPressureWatchOptionValue : SimpleGrammarOptionValues(
-    "config_parse_memory_pressure_watch",
+    "config_parse_pressure_watch",
     SequenceCombinator(
         FlexibleLiteralChoiceTerminal("yes", "no", "1", "0", "true", "false", "on", "off", "y", "n", "t", "f", "auto", "skip"),
         EOF()
